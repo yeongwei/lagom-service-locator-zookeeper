ade9d757e5765d91176af26c7ed311b61f72e732
YeongWei <laiyw@persistent.my>
2016-10-11 23:30:46 -0400

    Added test spec for Service Discovery instances with multiple ZooKeeper clients

--- src/test/scala/com/lightbend/lagom/discovery/zookeeper/MultipleZooKeeperServiceDiscoverySpec.scala
+++ src/test/scala/com/lightbend/lagom/discovery/zookeeper/MultipleZooKeeperServiceDiscoverySpec.scala
@@ -1,36 +1,34 @@
 package com.lightbend.lagom.discovery.zookeeper
 
-import akka.actor.ActorSystem
-import akka.testkit.TestKit
-import akka.testkit.TestProbe
-
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
 
-import org.scalatest.BeforeAndAfterAll
-import org.scalatest.FunSpec
+import scala.concurrent.duration.FiniteDuration
+
 import org.apache.curator.framework.CuratorFramework
-import org.apache.curator.test.TestingServer
-import org.apache.curator.x.discovery.ServiceDiscovery
-import org.apache.curator.x.discovery.details.ServiceDiscoveryImpl
 import org.apache.curator.framework.CuratorFrameworkFactory
-import org.apache.curator.retry.RetryForever
-import org.apache.curator.x.discovery.ServiceDiscoveryBuilder
-import org.apache.curator.framework.recipes.cache.TreeCacheListener
-import org.apache.curator.framework.recipes.cache.TreeCacheEvent
 import org.apache.curator.framework.recipes.cache.TreeCache
+import org.apache.curator.framework.recipes.cache.TreeCacheEvent
+import org.apache.curator.framework.recipes.cache.TreeCacheListener
+import org.apache.curator.retry.RetryForever
+import org.apache.curator.test.TestingServer
+import org.apache.curator.x.discovery.ServiceDiscoveryBuilder
 import org.apache.curator.x.discovery.ServiceInstance
 import org.apache.curator.x.discovery.UriSpec
+import org.apache.zookeeper.data.Stat
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.FunSpecLike
 
-import persistent.npm.service.registry.CuratorFrameworkApiSpec
-import persistent.npm.service.registry.ServiceDiscoverySpec
+import akka.actor.ActorSystem
+import akka.testkit.TestKit
+import akka.testkit.TestProbe
 import persistent.npm.service.registry.BaseSpec
+import persistent.npm.service.registry.CuratorFrameworkApiSpec
 
-import scala.concurrent.duration.FiniteDuration
 
 object MultipleZooKeeperServiceDiscoverySpec extends BaseSpec {
   def name = "MultipleZooKeeperServiceDiscoverySpec"
+  def finiteDuration = new FiniteDuration(10, TimeUnit.SECONDS)
 }
 
 class MultipleZooKeeperServiceDiscoverySpec extends TestKit(ActorSystem(MultipleZooKeeperServiceDiscoverySpec.name, MultipleZooKeeperServiceDiscoverySpec.configuration))
@@ -42,6 +40,7 @@ class MultipleZooKeeperServiceDiscoverySpec extends TestKit(ActorSystem(Multiple
   private val retryInterval = 3000
   private val counter = new AtomicInteger(0)
   private val testProbe = TestProbe()
+  private val stats = List[Stat]()
 
   private var zooKeeperServer: TestingServer = _
   private var client1: CuratorFramework = _
@@ -51,6 +50,7 @@ class MultipleZooKeeperServiceDiscoverySpec extends TestKit(ActorSystem(Multiple
   private def listener = new TreeCacheListener() {
     def childEvent(client: CuratorFramework, event: TreeCacheEvent) = {
       info(s"event[${counter.incrementAndGet()}]: ${event}")
+      event.getData.getStat
       testProbe.send(testProbe.ref, event)
     }
   }
@@ -66,6 +66,19 @@ class MultipleZooKeeperServiceDiscoverySpec extends TestKit(ActorSystem(Multiple
       .build
     serviceInstance
   }
+
+  private def assertForEvent(eventType: TreeCacheEvent.Type, path: String) = {
+    val msg = testProbe.expectMsgClass(MultipleZooKeeperServiceDiscoverySpec.finiteDuration, classOf[TreeCacheEvent])
+    info(s"WatchedEvent: $msg")
+    assert(msg.getType === eventType)
+    if (path == null)
+      assert(msg.getData === null)
+    else
+      assert(msg.getData.getPath.equals(path))
+  }
+  
+  private def createServiceDiscovery(_client: CuratorFramework, _baseServicePath: String) =
+        ServiceDiscoveryBuilder.builder(classOf[String]).client(_client).basePath(_baseServicePath).build();
 
   override def beforeAll = {
     zooKeeperServer = CuratorFrameworkApiSpec.zooKeeperServer
@@ -86,12 +99,20 @@ class MultipleZooKeeperServiceDiscoverySpec extends TestKit(ActorSystem(Multiple
       clientForCache.blockUntilConnected()
       assert(true)
     }
-    
-    it("should initialize one ZooKeeper Client for Service DIscovery"){
+
+    it("should initialize one ZooKeeper Client for Service DIscovery") {
       clientForServiceDiscovery = CuratorFrameworkFactory.newClient(zooKeeperServer.getConnectString, sessionTimeout, connectionTimeout, new RetryForever(retryInterval))
       clientForServiceDiscovery.start()
       clientForServiceDiscovery.blockUntilConnected()
       assert(true)
+    }
+    
+    it("should have different session id for both ZooKeeper Client instances") {
+      val client1 = clientForCache.getZookeeperClient.getZooKeeper.getSessionId
+      val client2 = clientForServiceDiscovery.getZookeeperClient.getZooKeeper.getSessionId
+      info(s"client1: ${client1}")
+      info(s"client2: ${client2}")
+      assert(client1 !== client2)
     }
 
     it("should start cache") {
@@ -99,15 +120,10 @@ class MultipleZooKeeperServiceDiscoverySpec extends TestKit(ActorSystem(Multiple
       cache.getListenable.addListener(listener)
       cache.start()
 
-      val msg = testProbe.expectMsgClass(new FiniteDuration(10, TimeUnit.SECONDS), classOf[TreeCacheEvent])
-      info(s"WatchedEvent: $msg")
-      assert(msg.getType === TreeCacheEvent.Type.INITIALIZED)
-      assert(msg.getData === null)
+      assertForEvent(TreeCacheEvent.Type.INITIALIZED, null)
     }
 
-    it("should create 2 instance of Discovery Service and do a couple of things") {
-      def createServiceDiscovery(_client: CuratorFramework, _baseServicePath: String) =
-        ServiceDiscoveryBuilder.builder(classOf[String]).client(_client).basePath(_baseServicePath).build();
+    it("should create 2 instance of Discovery Service and perform registration and unregistration") {
       val serviceDiscovery1 = createServiceDiscovery(clientForServiceDiscovery, baseServicePath)
       val serviceDiscovery2 = createServiceDiscovery(clientForServiceDiscovery, baseServicePath)
 
@@ -116,58 +132,100 @@ class MultipleZooKeeperServiceDiscoverySpec extends TestKit(ActorSystem(Multiple
 
       val service1 = newServiceInstance("serviceA", "1", "hostA", 1011)
       val service2 = newServiceInstance("serviceA", "2", "hostB", 1011)
-      
-      serviceDiscovery1.registerService(service1)
-      serviceDiscovery2.registerService(service2)
-      
+
+      serviceDiscovery1.registerService(service1) // should have node created events for basePath, servicePath and instancePath
+      serviceDiscovery2.registerService(service2) // should have node created event for instancePath
+
       serviceDiscovery1.unregisterService(service2) // should have no events at all because not
-      
-      serviceDiscovery1.unregisterService(service1) // should have events
-      serviceDiscovery2.unregisterService(service2) // should have events
-      
+
+      serviceDiscovery1.unregisterService(service1) // should have node removed event
+      serviceDiscovery2.unregisterService(service2) // should have node removed event
+
       assert(true)
     }
-    
-    it("should get node created for base service path") {
-      val msg = testProbe.expectMsgClass(new FiniteDuration(10, TimeUnit.SECONDS), classOf[TreeCacheEvent])
-      info(s"WatchedEvent: $msg")
-      assert(msg.getType === TreeCacheEvent.Type.NODE_ADDED)
-      assert(msg.getData.getPath.equals(baseServicePath))
+
+    it(s"should get node created for base service path ${baseServicePath}") {
+      assertForEvent(TreeCacheEvent.Type.NODE_ADDED, baseServicePath)
     }
-    
+
     it("should get node created for service A") {
-      val msg2 = testProbe.expectMsgClass(new FiniteDuration(10, TimeUnit.SECONDS), classOf[TreeCacheEvent])
-      info(s"WatchedEvent: $msg2")
-      assert(msg2.getType === TreeCacheEvent.Type.NODE_ADDED)
-      assert(msg2.getData.getPath.equals(s"${baseServicePath}/serviceA"))
+      assertForEvent(TreeCacheEvent.Type.NODE_ADDED, s"${baseServicePath}/serviceA")
     }
-    
+
     it("should get node created for service A id 1") {
-      val msg3 = testProbe.expectMsgClass(new FiniteDuration(10, TimeUnit.SECONDS), classOf[TreeCacheEvent])
-      info(s"WatchedEvent: $msg3")
-      assert(msg3.getType === TreeCacheEvent.Type.NODE_ADDED)
-      assert(msg3.getData.getPath.equals(s"${baseServicePath}/serviceA/1"))
+      assertForEvent(TreeCacheEvent.Type.NODE_ADDED, s"${baseServicePath}/serviceA/1")
     }
-    
+
     it("should get node created for service A id 2") {
-      val msg4 = testProbe.expectMsgClass(new FiniteDuration(10, TimeUnit.SECONDS), classOf[TreeCacheEvent])
-      info(s"WatchedEvent: $msg4")
-      assert(msg4.getType === TreeCacheEvent.Type.NODE_ADDED)
-      assert(msg4.getData.getPath.equals(s"${baseServicePath}/serviceA/2"))
+      assertForEvent(TreeCacheEvent.Type.NODE_ADDED, s"${baseServicePath}/serviceA/2")
     }
-    
+
     it("should get node removed for service A id 1") {
-      val msg5 = testProbe.expectMsgClass(new FiniteDuration(10, TimeUnit.SECONDS), classOf[TreeCacheEvent])
-      info(s"WatchedEvent: $msg5")
-      assert(msg5.getType === TreeCacheEvent.Type.NODE_REMOVED)
-      assert(msg5.getData.getPath.equals(s"${baseServicePath}/serviceA/1"))
+      assertForEvent(TreeCacheEvent.Type.NODE_REMOVED, s"${baseServicePath}/serviceA/1")
+    }
+
+    it("should get node removed for service A id 2") {
+      assertForEvent(TreeCacheEvent.Type.NODE_REMOVED, s"${baseServicePath}/serviceA/2")
+    }
+  }
+  
+  var clientA: CuratorFramework = _
+  var clientB: CuratorFramework = _
+  
+  describe("Two Service Discovery Instance with different ZooKeeper Client Instance") {
+    it("should initialize two different ZooKeeper Client instances") {
+      clientA = CuratorFrameworkFactory.newClient(zooKeeperServer.getConnectString, sessionTimeout, connectionTimeout, new RetryForever(retryInterval))
+      clientB = CuratorFrameworkFactory.newClient(zooKeeperServer.getConnectString, sessionTimeout, connectionTimeout, new RetryForever(retryInterval))
+      
+      clientA.start()
+      clientA.blockUntilConnected()
+      
+      clientB.start()
+      clientB.blockUntilConnected()
+      
+      val clientAid = clientA.getZookeeperClient.getZooKeeper.getSessionId
+      val clientBid = clientB.getZookeeperClient.getZooKeeper.getSessionId
+      
+      info(s"clientAid: ${clientAid}")
+      info(s"clientBid: ${clientBid}")
+      assert(clientAid != clientBid)
     }
     
-    it("should get node removed for service A id 2") {
-      val msg5 = testProbe.expectMsgClass(new FiniteDuration(10, TimeUnit.SECONDS), classOf[TreeCacheEvent])
-      info(s"WatchedEvent: $msg5")
-      assert(msg5.getType === TreeCacheEvent.Type.NODE_REMOVED)
-      assert(msg5.getData.getPath.equals(s"${baseServicePath}/serviceA/2"))
+    it("should create 2 instance of Discovery Service and perform registration and unregistration") {
+      val serviceDiscovery1 = createServiceDiscovery(clientA, baseServicePath)
+      val serviceDiscovery2 = createServiceDiscovery(clientB, baseServicePath)
+
+      serviceDiscovery1.start()
+      serviceDiscovery2.start()
+
+      val service1 = newServiceInstance("serviceB", "1", "host1", 1011)
+      val service2 = newServiceInstance("serviceB", "2", "host2", 1011)
+
+      serviceDiscovery1.registerService(service1) // should have node created events for basePath, servicePath and instancePath
+      serviceDiscovery2.registerService(service2) // should have node created event for instancePath
+      
+      serviceDiscovery1.unregisterService(service1) // should have node removed event
+      serviceDiscovery2.unregisterService(service2) // should have node removed event
+    }
+
+    it("should get node created for service B") {
+      assertForEvent(TreeCacheEvent.Type.NODE_ADDED, s"${baseServicePath}/serviceB")
+    }
+
+    it("should get node created for service B id 1") {
+      assertForEvent(TreeCacheEvent.Type.NODE_ADDED, s"${baseServicePath}/serviceB/1")
+    }
+
+    it("should get node created for service B id 2") {
+      assertForEvent(TreeCacheEvent.Type.NODE_ADDED, s"${baseServicePath}/serviceB/2")
+    }
+    
+    it("should get node removed for service B id 1") {
+      assertForEvent(TreeCacheEvent.Type.NODE_REMOVED, s"${baseServicePath}/serviceB/1")
+    }
+
+    it("should get node removed for service B id 2") {
+      assertForEvent(TreeCacheEvent.Type.NODE_REMOVED, s"${baseServicePath}/serviceB/2")
     }
   }
 }
