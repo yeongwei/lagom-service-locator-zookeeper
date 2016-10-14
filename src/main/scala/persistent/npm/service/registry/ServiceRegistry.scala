1116990a9f47b6440665d771b14ad55572802f50
YeongWei <laiyw@persistent.my>
2016-10-14 08:44:20 -0400

    Finalizing Service Registry with NGINX

--- src/main/scala/persistent/npm/service/registry/ServiceRegistry.scala
+++ src/main/scala/persistent/npm/service/registry/ServiceRegistry.scala
@@ -2,17 +2,27 @@ package persistent.npm.service.registry
 
 import akka.actor.ActorLogging
 import akka.actor.Actor
-import scala.collection.JavaConverters._
+import akka.actor.Props
+
+import java.io.File
+import java.io.PrintWriter
+
 import org.apache.curator.framework.recipes.cache.TreeCache
 import org.apache.curator.framework.CuratorFramework
 import org.apache.curator.framework.CuratorFrameworkFactory
 import org.apache.curator.retry.RetryForever
 import org.apache.curator.framework.recipes.cache.TreeCacheListener
 import org.apache.curator.framework.recipes.cache.TreeCacheEvent
-import scala.collection.mutable.HashMap
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable.{ HashMap => MutableHashMap }
+import scala.collection.immutable.{ HashMap => ImmutableHashMap }
+import scala.collection.immutable.{ Seq => ImmutableSeq }
 import scala.util.Try
 import scala.util.Failure
 import scala.util.Success
+import scala.sys.process.stringToProcess
+
 import spray.json.DefaultJsonProtocol
 import spray.json.RootJsonFormat
 import spray.json.JsValue
@@ -22,7 +32,6 @@ import spray.json.JsString
 import spray.json.JsObject
 
 protected trait ServiceInstanceJson {
-  case class ServiceInfo(state: TreeCacheEvent.Type)
   /*
    * {
    * 	"name":"service1","id":"1","address":"sherpavm","port":9009,"sslPort":null,"payload":null,
@@ -37,46 +46,85 @@ protected trait ServiceInstanceJson {
    */
   case class Part(value: String, variable: Boolean)
   case class UriSpec(parts: List[Part])
-  case class ServiceInsance(name: String, id: String, address: String, port: Int, sslPort: String, payload: String,
-                            registrationTimeUTC: Long, serviceType: String, uriSpec: UriSpec)
+  case class ServiceInstance(name: String, id: String, address: String, port: Int, sslPort: String, payload: String,
+                             registrationTimeUTC: Long, serviceType: String, uriSpec: UriSpec)
   object ServiceInstanceProtocol extends DefaultJsonProtocol {
     implicit val partFormat = jsonFormat2(Part)
     implicit val uriSpecFormat = jsonFormat1(UriSpec)
-    implicit object timeSeriesMetricEntryListFormat extends RootJsonFormat[ServiceInsance] {
+    implicit object timeSeriesMetricEntryListFormat extends RootJsonFormat[ServiceInstance] {
       def read(value: JsValue) = {
+        def getElseNull(func: () => String): String = Try(func()) match {
+          case Success(r) => r
+          case Failure(_) => null
+        }
+
         val jsonObject = value.asJsObject
-        val name = jsonObject.getFields("name").head.asInstanceOf[JsString].value
-        val id = jsonObject.getFields("id").head.asInstanceOf[JsString].value
-        val address = jsonObject.getFields("address").head.asInstanceOf[JsString].value
+
+        val name = getElseNull(() => jsonObject.getFields("name").head.asInstanceOf[JsString].value)
+        val id = getElseNull(() => jsonObject.getFields("id").head.asInstanceOf[JsString].value)
+        val address = getElseNull(() => jsonObject.getFields("address").head.asInstanceOf[JsString].value)
         val port = jsonObject.getFields("port").head.asInstanceOf[JsNumber].value.toInt
-        val sslPort = jsonObject.getFields("sslPort").head // handle null
-        val payload = jsonObject.getFields("payload").head // handle null
+        val sslPort = getElseNull(() => jsonObject.getFields("sslPort").head.asInstanceOf[JsString].value) // handle null
+        val payload = getElseNull(() => jsonObject.getFields("payload").head.asInstanceOf[JsString].value) // handle null
         val registrationTimeUTC = jsonObject.getFields("registrationTimeUTC").head.asInstanceOf[JsNumber].value.longValue()
-        val serviceType = jsonObject.getFields("payload") // handle null
+        val serviceType = getElseNull(() => jsonObject.getFields("payload").head.asInstanceOf[JsString].value) // handle null
         val uriSpec = jsonObject.getFields("uriSpec").head.convertTo[UriSpec]
-        ServiceInsance(name, id, address, port, null, null, registrationTimeUTC, null, uriSpec)
+
+        ServiceInstance(name, id, address, port, null, null, registrationTimeUTC, null, uriSpec)
       }
-      def write(z: ServiceInsance) = ???
+      def write(z: ServiceInstance) = ???
     }
 
   }
 }
 
-class ServiceRegistry(zooKeeperUrl: String, servicesBasePath: String) extends Actor with ActorLogging with ServiceInstanceJson {
+protected trait NginxConfiguration {
+  def configurationBaseDirectory = "/etc/nginx"
+  def confDdirectory = s"${configurationBaseDirectory}/conf.d"
+  def defaultDdirectory = s"${configurationBaseDirectory}/default.d"
+
+  def npiServerConf = s"${confDdirectory}/npi-server.conf"
+  def npiProxyConf = s"${defaultDdirectory}/npi-proxy.conf"
+}
+
+object ServiceRegistry {
+  /**
+   * e.g. npi-threshold -> [/services/thresholds/all, /services/threshold/get, ...]
+   */
+  def serviceMappings: ImmutableHashMap[String, ImmutableSeq[String]] =
+    ImmutableHashMap[String, ImmutableSeq[String]](
+      "service1" -> ImmutableSeq("/service1/api1", "/service1/api2"),
+      "service2" -> ImmutableSeq("/service2/api1", "/service2/api2"),
+      "service3" -> ImmutableSeq("/service3/api1", "/service3/api2"))
+
+  def props(zooKeeperUrl: String, servicesBasePath: String) = Props(new ServiceRegistry(zooKeeperUrl, servicesBasePath))
+}
+
+class ServiceRegistry(zooKeeperUrl: String, servicesBasePath: String) extends Actor with ActorLogging with ServiceInstanceJson with NginxConfiguration {
   import ServiceInstanceProtocol._
 
-  private val sessionTimeout = 5000
-  private val connectionTimeout = 5000
-  private val retryInterval = 5000
-  private val servicesState: HashMap[String, TreeCacheEvent.Type] = new HashMap[String, TreeCacheEvent.Type]() // e.g. /npi/services/threshold/1 -> NODE_ADDED
+  /**
+   * @param state the latest state, either added or updated
+   * @param instance the latest data representing a serviceInstance
+   */
+  private case class ServiceInfo(state: TreeCacheEvent.Type, instance: ServiceInstance)
+
+  private val _sessionTimeout = 5000
+  private val _connectionTimeout = 5000
+  private val _retryInterval = 5000
+  /**
+   * e.g. /npi/services/threshold/1 -> NODE_ADDED, ServiceInstance()
+   */
+  private val _servicesState: MutableHashMap[String, ServiceInfo] = new MutableHashMap[String, ServiceInfo]()
 
   private var _cache: TreeCache = null
   private var _zooKeeperClient: CuratorFramework = null
 
   override def preStart = {
-    _zooKeeperClient = CuratorFrameworkFactory.newClient(zooKeeperUrl, sessionTimeout, connectionTimeout, new RetryForever(retryInterval))
+    _zooKeeperClient = CuratorFrameworkFactory.newClient(zooKeeperUrl, _sessionTimeout, _connectionTimeout, new RetryForever(_retryInterval))
     _zooKeeperClient.start()
     _zooKeeperClient.blockUntilConnected()
+    log.info("ZooKeeper client started.")
 
     _cache = new TreeCache(_zooKeeperClient, servicesBasePath)
     _cache.getListenable.addListener(new TreeCacheListener() {
@@ -84,14 +132,18 @@ class ServiceRegistry(zooKeeperUrl: String, servicesBasePath: String) extends Ac
         cacheEventCallback(client, event)
     })
     _cache.start()
+    log.info("Curator Tree Cache started.")
 
     Try(_zooKeeperClient.getChildren.forPath(servicesBasePath)) match {
       case Success(servicePaths) => servicePaths.asScala.foreach { servicePath =>
         Try(_zooKeeperClient.getChildren.forPath(s"${servicesBasePath}/${servicePath}")) match {
           case Success(serviceInstancePaths) => serviceInstancePaths.asScala.foreach { serviceInstancePath =>
             Try(_zooKeeperClient.getData.forPath(s"${servicesBasePath}/${servicePath}/${serviceInstancePath}")) match {
-              case Success(data) => JsonParser(data.map(b => b.toChar).mkString("")).convertTo[ServiceInsance]
-              case Failure(f)    =>
+              case Success(data) => {
+                val serviceInstance = JsonParser(bytesToString(data)).convertTo[ServiceInstance]
+                _servicesState += s"${servicesBasePath}/${servicePath}/${serviceInstancePath}" -> ServiceInfo(TreeCacheEvent.Type.NODE_ADDED, serviceInstance)
+              }
+              case Failure(f) =>
             }
           }
           case Failure(f) =>
@@ -99,24 +151,175 @@ class ServiceRegistry(zooKeeperUrl: String, servicesBasePath: String) extends Ac
       }
       case Failure(f) =>
     }
+
+    log.info("{} service(s) registered.", _servicesState.size)
   }
 
+  private def bytesToString(bytes: Array[Byte]): String = bytes.map(b => b.toChar).mkString("")
+  // Only serviceInstance level has data, e.g. /npi/services/threshold/1
+  // e.g. WatchedEvent: TreeCacheEvent{type=NODE_ADDED, data=ChildData{path='/services/service1', stat=5,5,1476429387587,1476429387587,0,1,0,0,0,1,6, data=[]}}
   private def evaluate(event: TreeCacheEvent, evaluateFunc: (TreeCacheEvent) => Unit): Unit = {
-    if (event.getData != null)
-      evaluateFunc(event)
+    if (event.getData == null)
+      log.info("Event {} not evaluated.", event.getType.name())
+    else {
+      if (event.getData.getData.size > 0)
+        evaluateFunc(event)
+      else
+        log.info("Event {} for path {} not evaluated.", event.getType.name(), event.getData.getPath)
+    }
   }
-  private def cacheEventCallback(client: CuratorFramework, event: TreeCacheEvent): Unit = event.getType match {
-    case TreeCacheEvent.Type.NODE_ADDED   => handlNodeAdded(event)
+
+  /**
+   * Evaluate based on events, if true then reload service gateway
+   */
+  private def cacheEventCallback(client: CuratorFramework, event: TreeCacheEvent): Unit = cacheEventCallback(event) match {
+    case true  => reloadServiceGateway
+    case false => log.info("Do not reload service gateway for {}.", event)
+  }
+
+  private def cacheEventCallback(event: TreeCacheEvent): Boolean = event.getType match {
+    case TreeCacheEvent.Type.NODE_ADDED   => handleNodeAdded(event)
     case TreeCacheEvent.Type.NODE_REMOVED => handleNodeRemoved(event)
     case TreeCacheEvent.Type.NODE_UPDATED => handleNodeUpdate(event)
-    case _                                =>
+    case _ => {
+      log.info("Event {} is ignored.", event.getType.name())
+      false
+    }
   }
 
-  // e.g. /npi/services/threshold/1
-  private def getServiceName(path: String): String = path.replace(s"${servicesBasePath}/", "").split("/").head
-  private def handlNodeAdded(event: TreeCacheEvent): Unit = ???
-  private def handleNodeRemoved(event: TreeCacheEvent): Unit = ???
-  private def handleNodeUpdate(event: TreeCacheEvent): Unit = ???
+  // TODO: Need to revise the scope of this
+  /**
+   * Generate configurations for NGINX
+   * 1. npi-server.conf: serviceName -> serviceHostName + serviceHostPort
+   * 2. npi-proxy.conf: url -> protocol + serviceName
+   * 3. Directive location might need the = based on explanation at http://nginx.org/en/docs/http/ngx_http_core_module.html#location
+   */
+  private def reloadServiceGateway: Unit = {
+    // e.g. serviceName -> [serviceInstance]
+    val serviceToInstances = ServiceRegistry.serviceMappings.map {
+      case (serviceName, _) => serviceName ->
+        _servicesState.filter { case (serviceInstancePath: String, _) => serviceInstancePath.contains(s"/${serviceName}/") }
+        .map { case (_, serviceInfo) => serviceInfo }.toSeq
+    }
+
+    /*
+     * upstream sherpa2 {
+				server server1:80; # localhost:99; # server1
+        server server2:80; # localhost:88; # server2
+				}
+     * 
+     */
+    val servers = new StringBuffer()
+    serviceToInstances.foreach {
+      case (serviceName, serviceInstances) => {
+        servers.append(s"""upstream ${serviceName} {\n""")
+        serviceInstances.foreach { case ServiceInfo(_, instance) => servers.append(s"""    server ${instance.address}:${instance.port};\n""") }
+        servers.append(s"}\n")
+      }
+    }
+    log.info("npi-server.conf:\n{}", servers.toString())
+
+    /*
+     * location /service/api {
+				proxy_pass http://sherpa2/service/api/index.html;
+				}
+     */
+    val proxies = new StringBuffer()
+    ServiceRegistry.serviceMappings.map {
+      case (serviceName, uris) => {
+        uris.foreach { uri =>
+          proxies.append(s"""location ${uri} {\n""").append(s"""  proxy_pass http://${serviceName};\n""").append(s"}\n")
+        }
+      }
+    }
+    log.info("npi-proxy.conf:\n{}", proxies.toString())
+
+    def backupIfExist(fullFileName: String): Unit = {
+      val file = new File(fullFileName)
+      if (file.exists())
+        if (file.renameTo(new File(s"${fullFileName}.${System.currentTimeMillis()}")))
+          log.info(s"${file.getAbsolutePath} backup created")
+        else
+          log.info(s"${file.getAbsolutePath} backup not created")
+    }
+
+    backupIfExist(npiServerConf)
+    backupIfExist(npiProxyConf)
+
+    def writeToFile(fullFileName: String, content: String) = new PrintWriter(fullFileName) {
+      write(content)
+      close
+    }
+
+    writeToFile(npiServerConf, servers.toString())
+    writeToFile(npiProxyConf, proxies.toString())
+
+    log.info("About to reload nginx")
+    val ret = (s"systemctl reload nginx.service").!!
+    log.info(s"${ret}")
+  }
+
+  /**
+   * e.g. From /npi/services/threshold/1 to threshold
+   */
+  private def getServiceName(serviceInstancePath: String): String = serviceInstancePath.replace(s"${servicesBasePath}/", "").split("/").head
+
+  /**
+   * If found serviceInstance then do nothing else add to map
+   * @return true if added
+   */
+  private def handleNodeAdded(event: TreeCacheEvent): Boolean = {
+    val serviceInstancePath = event.getData.getPath
+    _servicesState.contains(serviceInstancePath) match {
+      case true => {
+        log.info("Service instance {} already exist.", serviceInstancePath)
+        false
+      }
+      case false => {
+        _servicesState += serviceInstancePath -> ServiceInfo(TreeCacheEvent.Type.NODE_ADDED,
+          JsonParser(bytesToString(event.getData.getData)).convertTo[ServiceInstance])
+        log.info("Service instance {} added.", serviceInstancePath)
+        true
+      }
+    }
+  }
+
+  /**
+   * Remove serviceInstance from map with serviceInstancePath
+   * @return true if removed
+   */
+  private def handleNodeRemoved(event: TreeCacheEvent): Boolean = {
+    val serviceInstancePath = event.getData.getPath
+    _servicesState.remove(serviceInstancePath) match {
+      case Some(serviceInfo: ServiceInfo) => {
+        log.info("Service instance {} removed.", serviceInstancePath)
+        true
+      }
+      case None => {
+        log.info("Service instance {} not found for removal.", serviceInstancePath)
+        false
+      }
+    }
+  }
+
+  /**
+   * If found serviceInstance then update map else do nothing
+   * @return true if updated
+   */
+  private def handleNodeUpdate(event: TreeCacheEvent): Boolean = {
+    val serviceInstancePath = event.getData.getPath
+    _servicesState.contains(serviceInstancePath) match {
+      case true => {
+        _servicesState.update(serviceInstancePath, ServiceInfo(TreeCacheEvent.Type.NODE_UPDATED,
+          JsonParser(bytesToString(event.getData.getData)).convertTo[ServiceInstance]))
+        true
+      }
+      case false => {
+        log.info("Service instance {} not found for update.", serviceInstancePath)
+        false
+      }
+    }
+  }
 
   def receive = {
     case _@ _ =>
--- src/test/scala/persistent/npm/service/registry/ServiceRegistrySpec.scala
+++ src/test/scala/persistent/npm/service/registry/ServiceRegistrySpec.scala
@@ -1,11 +1,24 @@
 package persistent.npm.service.registry
 
+import akka.testkit.TestKit
+import akka.actor.ActorSystem
+import akka.actor.ActorRef
+
 import org.scalatest.FunSpec
+import org.scalatest.FunSpecLike
+import org.scalatest.BeforeAndAfterAll
+import org.apache.curator.test.TestingServer
+
 import spray.json.JsonParser
 
-class ServiceRegistrySpec extends FunSpec with ServiceInstanceJson {
+object ServiceRegistrySpec extends BaseSpec {
+  def name = "ServiceRegistrySpec"
+}
+
+class ServiceRegistrySpec extends TestKit(ActorSystem(ServiceRegistrySpec.name, ServiceRegistrySpec.configuration))
+    with FunSpecLike with ServiceInstanceJson with BeforeAndAfterAll {
   import ServiceInstanceProtocol._
-  
+
   describe("Service Instance Json") {
     it("should be parsable") {
       val json = """
@@ -20,10 +33,52 @@ class ServiceRegistrySpec extends FunSpec with ServiceInstanceJson {
             {"value":":","variable":false},
             {"value":"servicePort","variable":true}]}}
         """
-      
-      val serviceInstance = JsonParser(json).convertTo[ServiceInsance]
+
+      val serviceInstance = JsonParser(json).convertTo[ServiceInstance]
       assert(serviceInstance.name.equals("service1"))
       assert(serviceInstance.uriSpec.parts.size == 5)
+    }
+  }
+
+  private val servicesBasePath = "/npi/services"
+
+  private var server1: ActorRef = _
+  private var server2: ActorRef = _
+  private var server3: ActorRef = _
+  private var server4: ActorRef = _
+  private var server5: ActorRef = _
+  private var server6: ActorRef = _
+  
+  private var serviceRegistry: ActorRef = _
+  private var zooKeeperServer: TestingServer = _
+
+  override def beforeAll = {
+    zooKeeperServer = CuratorFrameworkApiSpec.zooKeeperServer
+    zooKeeperServer.start()
+    Thread.sleep(3000)
+  }
+  override def afterAll = {}
+
+  describe("Service Registry") {
+    it("should receive events after services registered") {
+      serviceRegistry = system.actorOf(ServiceRegistry.props(zooKeeperServer.getConnectString, servicesBasePath))
+      
+      server1 = system.actorOf(MockService.props(zooKeeperServer.getConnectString, servicesBasePath,
+        "service1", "1", "server1", 7001, true), "server1")
+      server2 = system.actorOf(MockService.props(zooKeeperServer.getConnectString, servicesBasePath,
+        "service1", "2", "server2", 7002, true), "server2")
+        
+      server3 = system.actorOf(MockService.props(zooKeeperServer.getConnectString, servicesBasePath,
+        "service2", "1", "server3", 8001, true), "server3")
+      server4 = system.actorOf(MockService.props(zooKeeperServer.getConnectString, servicesBasePath,
+        "service2", "2", "server4", 8001, true), "server4")
+        
+      server5 = system.actorOf(MockService.props(zooKeeperServer.getConnectString, servicesBasePath,
+        "service3", "1", "server5", 9001, true), "server5")
+      server6 = system.actorOf(MockService.props(zooKeeperServer.getConnectString, servicesBasePath,
+        "service3", "2", "server6", 9001, true), "server6")
+        
+      Thread.sleep(30000)
     }
   }
 }
